7f8f75b5f1934fdd0c4fc3eb7a9824e7e542dee1
Roger Hutchings <roger@hutchin.gs>
2016-03-11 15:53:09 +0000

    Try sending back the homepage

--- heroku-server.js
+++ heroku-server.js
@@ -12,9 +12,17 @@ var PORT = process.env.PORT || 8080;
 // App
 var app = express();
 
+// Provide logging with Morgan
 app.use(morgan('dev'));
 
-app.use('/', express.static('./public/build'));
+// Simply serves up the build folder on GET. This is only for testing, as 
+// Facebook requires a POST in order to do a signed request.
+// app.use('/', express.static('./public/build'));
+
+// Try sending back the home page
+app.post('/', function(req, res, next) {
+    res.render('./public/build/index.html');
+});
 
 app.listen(PORT, function () {
     console.log('Running on http://localhost:' + PORT);
