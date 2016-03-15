26efd055d650e11aeb21a01186c922cf49bb1e43
Roger Hutchings <roger@hutchin.gs>
2016-03-15 14:54:46 +0000

    Differentiate between local server (with unsigned certs) and heroku

--- local-server.js
+++ local-server.js
@@ -1,7 +1,6 @@
-// Server file for running on Heroku. Probably won't be used in production, but
-// simplifies the dev / testing process.
+// Server file for running locally.
 
-console.log('Running Heroku server...');
+console.log('Running local server...');
 
 var fs = require('fs');
 var https = require('https');
@@ -10,6 +9,7 @@ var morgan = require('morgan');
 
 // Constants
 var PORT = process.env.PORT || 8080;
+var HOST = process.env.HOST || '';
 
 // App
 var app = express();
@@ -26,6 +26,11 @@ app.post('/', function(req, res, next) {
     res.sendFile(__dirname + '/public/build/index.html');
 });
 
-app.listen(PORT, function () {
-    console.log('Running on http://localhost:' + PORT);
+var options = {
+    key: fs.readFileSync('./cert/key.pem'),
+    cert: fs.readFileSync('./cert/cert.pem'),
+};
+
+https.createServer(options, app).listen(PORT, HOST, null, function() {
+    console.log('Server listening on port %d in %s mode', this.address().port, app.settings.env);
 });
